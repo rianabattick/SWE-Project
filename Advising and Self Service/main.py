58eb14ed19ac0901b046eda93637ad184c80d312
NBuckle <97976536+NBuckle@users.noreply.github.com>
2023-12-12 16:06:04 -0900

    Update main.py

--- Advising and Self Service/main.py
+++ Advising and Self Service/main.py
@@ -21,6 +21,49 @@ def prereqDictionary(file_path):
   return course_dict
 
 
+def add_course(course_name, prereqs, course_dict):
+  new_prerequisites = prereqs.strip().split(',')  #split by commas
+  course_dict[course_name] = new_prerequisites
+  with open('CoursePrerequisites.txt', 'w') as file:
+    for course, prerequisites in course_dict.items():
+      file.write(f"{course}| {', '.join(prerequisites)}\n")
+    #NEW
+  return open('CoursePrerequisites.txt', mode='r')
+
+
+def edit_course(course_name, choice, prereqs, course_dict):
+  existing_course = course_name.strip()
+  if existing_course in course_dict:
+    print(
+        f"Current prerequisites for {existing_course}: {', '.join(course_dict[existing_course])}"
+    )
+    edit_choice = choice.upper()
+    if edit_choice == 'A':
+      new_prerequisites = prereqs.strip().split(',')
+      course_dict[existing_course].extend(
+          new_prerequisites)  #adds new course to course dictionary
+    elif edit_choice == 'B':
+      remove_prerequisites = prereqs.strip().split(',')
+      course_dict[existing_course] = [
+          prereq for prereq in course_dict[existing_course]
+          if prereq not in remove_prerequisites
+      ]  #only prints wanted courses to file
+    else:
+      print(
+          "Invalid choice. Please enter 'A' to add or 'B' to remove prerequisites."
+      )
+  else:
+    print(f"Course '{existing_course}' not found in the dictionary.")
+
+
+# Write the updated information to 'CoursePrerequisites.txt'
+  with open('CoursePrerequisites.txt', 'w') as file:
+    for course, prerequisites in course_dict.items():
+      file.write(f"{course}| {', '.join(prerequisites)}\n")
+
+  return open('CoursePrerequisites.txt', mode='r')
+
+
 def rubrikDictionary(file_path):
   rubrik_dict = {}
   with open(file_path, 'r') as file:
@@ -44,11 +87,9 @@ def rubrikDictionary(file_path):
 
 def find_matching_courses(course_dict, search_terms):
   matching_courses = set()
-
-  for term in search_terms:
-    for course, prerequisites in course_dict.items():
-      if term in prerequisites:
-        matching_courses.add(course)
+  for course, prerequisites in course_dict.items():
+    if search_terms in prerequisites:
+      matching_courses.add(course)
   if len(matching_courses) > 0:
     return matching_courses
   return "Course not found in dictionary"
@@ -57,7 +98,7 @@ def find_matching_courses(course_dict, search_terms):
 def print_prerequisites(course_dict, course_name):
   if course_name in course_dict:
     prerequisites = course_dict[course_name]
-    print(f"Prerequisites for {course_name}:")
+    print(f"Prerequisites for {course_name} are:")
     for prerequisite in prerequisites:
       print(prerequisite)
   else:
@@ -133,72 +174,79 @@ def get_employee_email(employee_dict, employee_name):
     print(f"Employee '{employee_name}' not found in the dictionary.")
 
 
+#-----------------------ADD OR EDIT COURSE
+print("DEMONSTRATING ADD COURSE: \n")
+course_dict = prereqDictionary('CoursePrerequisites.txt')
+course_name = input("Enter course name: ")
+prereqs = input("Enter prerequisites separated by comma: ")
+madout = add_course(course_name, prereqs, course_dict)
+#madout = add_or_edit_course(course_dict)
+print("\n\nHere is modified Course File")
+for line in madout:
+  print(line)
+print("\n\n\n")
+
+print("DEMONSTRATING EDIT COURSE: \n")
+course_name = input("Enter course name: ")
+choice = input("A for add B for Delete: ")
+prereqs = input("Enter prerequisites separated by comma: ")
+madout = edit_course(course_name, choice, prereqs, course_dict)
+print("\n\n")
+for line in madout:
+  print(line)
+print("\n\n\n")
+
 #____________________Search what class have search class as a prequisite
-file_path = 'CoursePrerequisites.txt'
+#file_path = 'CoursePrerequisites.txt'
+file_path = 'PrerequisitesClass.txt'
 course_dict = prereqDictionary(file_path)
 #search_terms = ['CSCI 136 Computer Science II']
 #you enter the course youve done to check what courses have it as a prerequiste
+print("DEMONSTRATING Course Satisfies prerequisites : \n")
 search_terms = input(
-    "To find what classes have a specific class as a prequisite, please enter the name of the desired class\nEnter full name (eg. CSCI 136 Computer Science II):  "
+    "To find what classes have a specific class as a prequisite, please enter the name of the desired class\nEnter abbreviated name (eg. CSCI 136 = Computer Science II):  "
 )
 matching_courses = find_matching_courses(course_dict, search_terms)
 # Print the matching courses
-print(f"Courses that have {search_terms} as a prerequisite:")
 if matching_courses == "Course not found in dictionary":
   print("Course not found in dictionary")
 else:
+  print(f"Courses that have {search_terms} as a prerequisite:")
   for course in matching_courses:
     print(course)
 
+print("\n\n")
 #---------------------------------TEST PREREQUISITES WHEN GIVEN A CLASS
 #search_class = 'CSCI 135 Computer Science I'
-#course_dict = prereqDictionary(file_path)
-
+print("DEMONSTRATING SEARCH PREREQUISITES FOR A COURSE: \n")
+file_path = 'SearchClass.txt'
+course_dict = prereqDictionary(file_path)
 search_class = input(
-    "Please type the name of a course you want to see the prequisites for.\nEnter full name (e.g CSCI 136 Computer Science II) : "
+    "Please type the name of a course you want to see the prequisites for.\nEnter abbreviated name (e.g CSCI 136 = Computer Science II) : "
 )
-print(f"The prerequisites for class {search_class} are: ")
+#print(f"The prerequisites for class {search_class} are: ")
 print_prerequisites(course_dict, search_class)
-
+print('\n\n')
 #------------------TEST Teacher CLASS ASSIGNMENT TEXT
-#file_path = 'ClassAssignments.txt'
 teacher_dict = course_teachers('ClassAssignments.txt')
 #for course, teachers in teacher_dict.items():
 # print(f'{course}: {teachers}')
 #--------------------find teacher
+print("DEMONSTRATING SEARCH TEACHERS ASSIGNED TO A COURSE: \n")
+
 search_course = input(
     "Please type course you are searching teachers for: (Use course abbreviation eg. CSCI 135 = Computer Science I)\nEnter course here: "
 )
 search_course = search_course.upper()
 print_course_teachers(teacher_dict, search_course)
-
+print('\n\n')
 #----------------------------------SEARCH FOR TEACHER EMAIL
+print("DEMONSTRATING SEARCH TEACHER INFORMATION: \n")
+
 search_teacher = input(
-    "Would you like to search for a teacher's email?\nEnter 1 for yes, 2 for no"
-)
-if (search_teacher == 1):
-  make_employee_dict('EmployeeInfo.txt')
+    "Would you like to search for a teacher's email?\nEnter A for yes, B for no\nEnter choice here: "
+).upper()
+if search_teacher == 'A':
+  employee_dict = make_employee_dict('EmployeeInfo.txt')
   find_teacher = input("Please type the teacher's name: ")
-  print(get_employee_email, find_teacher)
-
-#-----------------------------COURSE CALENDER
-print("Here is a list of major upcoming events for Spring 2024:\nMarch 25th - Fall 2024 Registration Begins\nApril 5th - Last day to Withdraw from Spring 2024 Course\nApril 12th - Last day to apply for Spring 2024 Graduation\nApril 23rd to 25th - Final Examinations for Prospective Spring 2024 Graduates\nApril 26th - Formal Classses Finish\nApril 29th to May 8th - Final Examinations\n\n")
-academic_calendar_link = "https://howard.edu/sites/home.howard.edu/files/2023-04/2023-2024AcademicCalendar.pdf"
-print(
-    f"Click the following link to view the full academic calendar: {academic_calendar_link}"
-)
-###
-######file_path = 'CoursePrerequisites.txt'
-#####result_dict = prereqDictionary(file_path)
-
-#####file_path = 'rub.txt'
-###rubrik_dict = rubrikDictionary(file_path)
-# Print all values for each key in dictionary
-###for course, prerequisites in result_dict.items():
-##print(f'{course}: {prerequisites}')
-#print(result_dict)
-
-###print("+++++++++++++++++++++++++++++++++++++++++++++++\n")
-##for course, credits in rubrik_dict.items():
-##print(f'{course}: {credits}')
-#print(result_dict)
+  get_employee_email(employee_dict, find_teacher)
